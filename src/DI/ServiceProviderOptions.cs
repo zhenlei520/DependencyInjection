21755536d1f81d5a7a0a9268dbd3040482e54b7b
磊磊 <839238345@qq.com>
2020-11-09 01:17:02 +0800

    調整注釋

--- src/DI/ServiceProviderOptions.cs
+++ src/DI/ServiceProviderOptions.cs
@@ -16,9 +16,13 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <summary>
         /// <c>true</c> to perform check verifying that scoped services never gets resolved from root provider; otherwise <c>false</c>.
         /// 是否需要检查生命周期
+        /// 此属性为true,不能从获取顶级容器中的scoped
         /// </summary>
         public bool ValidateScopes { get; set; }
 
+        /// <summary>
+        /// 实例化ServiceProvider模式,当前只能使用Dynamic模式
+        /// </summary>
         internal ServiceProviderMode Mode { get; set; } = ServiceProviderMode.Dynamic;
     }
 }
